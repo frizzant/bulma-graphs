cc6d46a411089dbba99e00f69361cecd5ebd81e3
erin mcgowan <erindazil@gmail.com>
2019-12-04 16:56:14 +0100

    Version 0.1.1

--- graphs.js
+++ graphs.js
@@ -1,26 +1,25 @@
 graphs = () => {
 // --- Variables + Objects
 // ---
-    let graphs = { // initialize object with settings
-        settings: {
-            containerSelector: '.graphs-container',
-            containerItemSelector: '.graphs-container div',
-            autoGenerate: true,
-            animation: true,
-        },
+            let graphs = { // initialize object with settings
+                settings: {
+                    containerSelector: '.graphs-container',
+                    containerItemSelector: '.graphs-container .data-container div',
+                    autoGenerate: true,
+                    animation: true,
+                    heightMultiplicator: 3, // scales in pixel
+                    widthMultiplicator: 1, // scales in % (percentage)
+                },
 
-    };
-    graphs = { // dependent on settings before they can be set
-        container: {
-            list: document.querySelectorAll(graphs.settings.containerSelector),
-            items: {
-                list: document.querySelectorAll(graphs.settings.containerItemSelector),
-            }
-        }
-    };
+            };
+            graphs.container = { // dependent on settings before they can be set
+                list: document.querySelectorAll(graphs.settings.containerSelector),
+                items: {
+                    list: document.querySelectorAll(graphs.settings.containerItemSelector),
+                }
+            };
 // ---
 // --- END Variables
-
     let main = () => {
         console.log(graphs.container.list);
 
@@ -29,9 +28,12 @@ graphs = () => {
                 let i = 0;
                 for (let container of graphs.container.list) {
                     container.id = 'graph-container-' + i;
-                    container.classList.add('content');
+                    container.classList.add('content', 'columns', 'is-multiline');
 
-                    container.insertAdjacentHTML('afterbegin', `<h2>${container.dataset.title}</h2>`);
+                    if (container.dataset.title.length >= 1) {
+                        container.insertAdjacentHTML('afterbegin', `<h2 class="column is-full">${container.dataset.title}</h2>`);
+                    }
+
                     i++;
                     if (i === graphs.container.list.length) {
                         resolve();
@@ -41,9 +43,29 @@ graphs = () => {
         };
         let generateHTMLItems = () => {
             return new Promise((resolve) => { // setting up a PROMISE
+                let rotateBase = 0;
+                let rotateFraction = 360 / graphs.container.items.list.length;
+
                 let i = 0;
                 for (let item of graphs.container.items.list) {
-                    console.log(item.dataset.title);
+                    // console.log(item.dataset.title);
+                    if (item.dataset.title.length >= 1) {
+                        item.insertAdjacentHTML('afterbegin', `<p>${item.dataset.title}</p>`);
+                    }
+
+                    if (item.parentElement.classList.contains('columns')) {
+                        item.classList.add('column');
+                    }
+
+                    if (item.parentElement.classList.contains('is-vertical-graph')) { // Vertical Graphic
+                        item.style.width = graphs.settings.widthMultiplicator * item.dataset.value + '%';
+                    } else { // Horizontal Graphic
+                        item.style.height = graphs.settings.heightMultiplicator * item.dataset.value + 'px';
+                    }
+
+                    item.style.filter = `hue-rotate(${rotateBase}deg)`;
+                    rotateBase = rotateBase + rotateFraction;
+
                     i++;
                     if (i === graphs.container.items.list.length) {
                         resolve();
